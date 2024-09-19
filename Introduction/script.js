076b34b0c44e25b4dfedbdb0b2728810f569c375
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-09-19 16:03:11 +0530

    Javascript learning

---
+++ Introduction/script.js
@@ -0,0 +1,33 @@
+ //This is an alert
+//Single line comment
+/*
+Multi line comment
+    */
+//variables-8bytes
+let score= 0 //initilization
+//assignment
+score = 10.5 
+//score = 'no score'
+
+const pi = 3.14
+
+console.log("score is " + score)
+
+
+/*Arithmetic Operators:+ - * ** /% ++ --
+Assignment Operators: = += -= /= %= **=
+Comparison Operators: == === !== > < >= <= ?:
+Logical Operators:&&  || !
+Bitwise Operators: & | ~  << >>  >>>
+*/
+
+//user input
+//let name = prompt("what's your name?")
+//console.log('hi',name)
+
+//let tickets = prompt('how many tickets do you want?')
+//console.log("you should pay", Number (tickets)*20)
+ 
+
+let a = prompt("Enter a number")
+console.log(Number(a)+10)

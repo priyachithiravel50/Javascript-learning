ba40c546a3d29a8f3483be60ba46a4a114dc956b
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-09-26 16:05:57 +0530

    Today updates

--- Javascript programming/demo.js
+++ Javascript programming/demo.js
@@ -1,26 +1,3 @@
-//if else statement
-var score = 50
 
-if (score<= 50) {
-    console.log("You need to improve");
-}
 
-else if(score>50 && score<=70)
-{
-    console.log("Good Job")
-}
 
-else if (score>70) {
-    console.log("Excellentm Performance");
-}
-
-//For loop
-for(count=1;count<=5;count=count+1)
-{
-    console.log("John")
-}
-
-for(count=1;count<=10;count=count+1)
-{
-    console.log("count")
-}

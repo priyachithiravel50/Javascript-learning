0b1ea6573d63b697512e547bd487107051b0c962
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-09-27 16:05:52 +0530

    Today Updates

--- Javascript programming/demo.js
+++ Javascript programming/demo.js
@@ -1,3 +1,9 @@
 
 
 
+let a = 5;
+let b = 10;
+
+console.log(a > 3 && b < 15); // true (both conditions are true)
+console.log(a > 6 && b < 15); // false (first condition is false)
+console.log(a > 3 && b > 15); // false (second condition is false)

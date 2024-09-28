c7bde7b3e3d7d4a0bb952d5a4445de1af2f0db3d
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-09-28 16:01:48 +0530

    Today Updates

--- Javascript programming/demo.js
+++ Javascript programming/demo.js
@@ -1,9 +1,12 @@
 
 
 
-let a = 5;
-let b = 10;
+// // Create an Object:
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   }
 
-console.log(a > 3 && b < 15); // true (both conditions are true)
-console.log(a > 6 && b < 15); // false (first condition is false)
-console.log(a > 3 && b > 15); // false (second condition is false)
+//   console.log(person)

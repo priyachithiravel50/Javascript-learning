440d6ae223722107d62ca247b0ac6b3d991dcbb5
priyachithiravel50 <priyachithiravel50@gmail.com>
2024-10-03 16:03:34 +0530

    Today Updates

---
+++ Introduction/color flipper/app.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn")
+const colortext = document.getElementById("color")
+const wrap = document.getElementById("wrap")
+const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+
